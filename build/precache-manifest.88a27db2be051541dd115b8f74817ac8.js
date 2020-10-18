c26f16f18385afe01d89147c798d7971ad46a5a1
2ksoft <duchanhstyle@gmail.com>
2020-10-18 09:43:15 +0700

    build

---
+++ build/precache-manifest.88a27db2be051541dd115b8f74817ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eed4cb94df0a7183cb81f8b49301414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d00274837a181bad359a",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "14b29af934cd6520fef0",
+    "url": "/static/css/main.be6fcd9d.chunk.css"
+  },
+  {
+    "revision": "d00274837a181bad359a",
+    "url": "/static/js/2.10eddaa1.chunk.js"
+  },
+  {
+    "revision": "8fa7e390ea50d6293cd549eae55e3e29",
+    "url": "/static/js/2.10eddaa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b29af934cd6520fef0",
+    "url": "/static/js/main.4fdd27f1.chunk.js"
+  },
+  {
+    "revision": "1400792d44b5d93da560",
+    "url": "/static/js/runtime-main.5e059319.js"
+  }
+]);
