cbeec7d6f4b0c390a05c5c7cdfcd5a7c34413575
2ksoft <duchanhstyle@gmail.com>
2020-10-18 10:03:12 +0700

    added landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import AceEditor from "react-ace";
 
 const index = () => {
-  return <h1>Welcome to DuoCode!</h1>;
+  function onChange(newValue) {
+    console.log("change", newValue);
+  }
+  return (
+    <Container>
+      <h1>Welcome to DuoCode!</h1>
+      <Row>
+        <Col>
+          <img
+            style={{ width: "500px" }} alt="helli"
+            src="https://2.bp.blogspot.com/-m-DkBUUtO_Q/Wc9WmTlpAZI/AAAAAAAAAKs/cjg467rHbc01oeW3zOANgYwdLyqMuDOSwCLcBGAs/s1600/lmbs_pairprogramming_co.png"
+          ></img>
+        </Col>
+        <Col>
+          <AceEditor
+            style={{boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)" }}
+            placeholder="Play with code !"
+            mode="javascript"
+            theme="monokai"
+            fontSize={20}
+            onChange={onChange}
+            getLabel={function (date) {
+              return date.slice(0, 4);
+            }}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default index;
